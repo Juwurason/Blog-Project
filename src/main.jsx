1c791717384798690be53fd267c41d01434d11e0
Juwurason <ajibolasunday7@gmail.com>
2022-09-28 12:32:46 +0100

    Log in and Signup

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './Pages/App'
-import Login from "./Pages/Login"
+import Login from "./Pages/Login/Login"
 import Notfound from './Pages/Notfound'
-import Signup from './Pages/Signup'
-
+import Signup from './Pages/Signup/Signup'
+// import "./Pages/styles.css"
 const router = createBrowserRouter(
  [
   {path: '/', element: <App />, errorElement: <Notfound /> },
